f79a2715f7cbec4e0f36991fb4959215b39560aa
Said <46088969+webmastersaid@users.noreply.github.com>
2023-11-03 12:43:47 +0500

    logout

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,4 +1,6 @@
 <script>
+import api from '../api'
+
 export default {
     data() {
         return {
@@ -8,6 +10,13 @@ export default {
     methods: {
         getAccessToken() {
             this.accessToken = localStorage.getItem('access_token')
+        },
+        logout() {
+            api.post('/api/auth/logout')
+            .then(res => {
+                localStorage.removeItem('access_token')
+                this.$router.push({name: 'user.login'})
+            })
         }
     },
     mounted() {
@@ -35,6 +44,9 @@ export default {
             </li>
             <li class="nav-item" v-if="accessToken">
                 <RouterLink class="nav-link" :to="{ name: 'user.person' }">You</RouterLink>
+            </li>
+            <li class="nav-item" v-if="accessToken">
+                <a @click.prevent="logout" href="#" class="nav-link">Logout</a>
             </li>
         </ul>
         <RouterView :key="$route.fullPath" class="pt-5"></RouterView>
